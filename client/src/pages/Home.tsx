cb87fc08c5744e31c6f97bd03439225506546246
Mukul Sharma <mukulsharma262002@gmail.com>
2024-02-23 23:48:04 +0530

    homepage + redux

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,6 +1,12 @@
 const Home = () => {
   return (
-    <div>Home</div>
-  )
-}
-export default Home
+    <div className="w-full h-[calc(100dvh-60px)] text-white flex justify-center items-center gap-5 flex-col">
+      <h1 className="text-center text-6xl font-bold">ONLINE CODE EDITOR</h1>
+      <p className="font-center font-medium text-xl text-gray-500">
+        Compile HTML, CSS and JavaScript on the go. Share with friends and
+        community.
+      </p>
+    </div>
+  );
+};
+export default Home;
